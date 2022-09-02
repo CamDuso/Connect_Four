37ddeba9848626c709318ab5dcf3cfcedfac9479
Cam <camduso8@gmail.com>
2022-09-02 13:10:33 -0400

    HTML and CSS for Project One

---
+++ App.js
@@ -0,0 +1,10 @@
+// Game Initialization
+
+// Yellow will go first
+// On turn, yellow will pick a slot to place their token
+// Token must fall to the furthest down slot in their respective row (No floating tokens)
+// After yellow has placed their piece, it will change to Red's turn
+// Red will follow the same steps, ensuring tokens fall to the "floor" of the gameboard
+// Repeat until Either red or yellow has 4 in a row, displaying a "____ Wins!" Message on screen, as well as a "New Game" Button
+
+// If No winners are declared by the time the board is full, then a "Game Over" message will display, also prompting players to play "New Game
